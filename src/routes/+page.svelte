5d4f90219f76e3a019e2742bae2c48c7d5606821
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-10-26 21:59:02 +0200

    styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,3 +9,15 @@
 	<h1>Hello {name.toUpperCase()}</h1>
 	<img {src} alt="{alt} logo" />
 </main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	img {
+		width: 100px;
+	}
+</style>
