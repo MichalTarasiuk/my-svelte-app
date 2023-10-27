0226fe2f5a55830ffab38ae393d91a5d4098ea36
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-10-27 17:42:37 +0200

    spread props

--- src/routes/props/+page.svelte
+++ src/routes/props/+page.svelte
@@ -1,5 +1,13 @@
 <script>
 	import ComponentWithProps from '$lib/components/componentWithProps.svelte';
+	import PackageInfo from '$lib/components/packageInfo.svelte';
+
+	const pkg = {
+		name: 'svelte',
+		speed: 'blazing',
+		version: 4,
+		website: 'https://svelte.dev'
+	};
 </script>
 
 <main>
@@ -11,5 +19,9 @@
 	<section>
 		<h2>Default values</h2>
 		<ComponentWithProps />
+	</section>
+	<section>
+		<h2>Spread props</h2>
+		<PackageInfo {...pkg} />
 	</section>
 </main>
