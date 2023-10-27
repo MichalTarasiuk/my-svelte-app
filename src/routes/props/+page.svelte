10dde68da07aa320aad5df664ee9667ce5b79a23
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-10-27 16:49:38 +0200

    add component with props

---
+++ src/routes/props/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import ComponentWithProps from '$lib/components/componentWithProps.svelte';
+</script>
+
+<main>
+	<h1>Props</h1>
+	<section>
+		<h2>Declaring props</h2>
+		<ComponentWithProps prop="value" />
+	</section>
+</main>
