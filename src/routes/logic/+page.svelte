3d5ad299464eb0e3b3514f7e1e433633a3e3e855
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-10-27 18:27:08 +0200

    add color picker component

---
+++ src/routes/logic/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	import ColorPicker from '$lib/components/colorPicker.svelte';
+	import Counter from '$lib/components/counter.svelte';
+</script>
+
+<main>
+	<h1>Logic</h1>
+	<section>
+		<h2>If blocks, Else blocks and Else-if blocks</h2>
+		<Counter />
+	</section>
+	<section>
+		<h2>Each blocks</h2>
+		<ColorPicker />
+	</section>
+</main>
