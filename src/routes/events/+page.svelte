1a4f1eedb03c678c9474a0dfbe1b79c25bde70cb
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-10-28 20:58:54 +0200

    add clickOnceBtn component

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import ClickOnceBtn from '$lib/components/clickOnceBtn.svelte';
 	import PointerMoveDisplay from '$lib/components/pointerMoveDisplay.svelte';
 </script>
 
@@ -7,5 +8,9 @@
 	<section>
 		<h2>DOM Events</h2>
 		<PointerMoveDisplay />
+	</section>
+	<section>
+		<h2>Event modifiers</h2>
+		<ClickOnceBtn />
 	</section>
 </main>
