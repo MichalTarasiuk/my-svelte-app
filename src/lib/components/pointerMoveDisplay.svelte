487bfc9d98d8fc0ffb314ac27d671ca5e716d2a5
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-10-28 20:47:24 +0200

    add PointerMoveDisplay component

---
+++ src/lib/components/pointerMoveDisplay.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import type { MouseEventHandler } from 'svelte/elements';
+
+	let m = { x: 0, y: 0 };
+
+	const handleMove: MouseEventHandler<HTMLDivElement> = (event) => {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	};
+</script>
+
+<p>The pointer is at {m.x} x {m.y}</p>
+
+<div on:mousemove={handleMove} role="presentation" />
+
+<style>
+	div {
+		position: absolute;
+		inset: 0;
+	}
+</style>
---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import PointerMoveDisplay from '$lib/components/pointerMoveDisplay.svelte';
+</script>
+
+<main>
+	<h1>Events</h1>
+	<section>
+		<h2>DOM Events</h2>
+		<PointerMoveDisplay />
+	</section>
+</main>
