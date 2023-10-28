dab3f3a0873a7c628fa0c845a8283af34913e6fc
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-10-28 22:12:49 +0200

    add numericInput component

--- src/routes/bindings/+page.svelte
+++ src/routes/bindings/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import NumericInput from '$lib/components/numericInput.svelte';
 	import TextInput from '$lib/components/textInput.svelte';
 </script>
 
@@ -7,5 +8,9 @@
 	<section>
 		<h2>Text inputs</h2>
 		<TextInput />
+	</section>
+	<section>
+		<h2>Numeric inputs</h2>
+		<NumericInput />
 	</section>
 </main>
