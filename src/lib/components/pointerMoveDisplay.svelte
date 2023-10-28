b48d8c6ee5e801588143741a00f88c1b68d319b9
MichalTarasiuk <tarasiuk.michal03@gmail.com>
2023-10-28 20:49:16 +0200

    add inline handler

--- src/lib/components/pointerMoveDisplay.svelte
+++ src/lib/components/pointerMoveDisplay.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
-	import type { MouseEventHandler } from 'svelte/elements';
+	// import type { MouseEventHandler } from 'svelte/elements';
 
 	let m = { x: 0, y: 0 };
 
-	const handleMove: MouseEventHandler<HTMLDivElement> = (event) => {
-		m.x = event.clientX;
-		m.y = event.clientY;
-	};
+	// const handleMove: MouseEventHandler<HTMLDivElement> = (event) => {
+	// 	m.x = event.clientX;
+	// 	m.y = event.clientY;
+	// };
 </script>
 
 <p>The pointer is at {m.x} x {m.y}</p>
 
-<div on:mousemove={handleMove} role="presentation" />
+<div
+	on:pointermove={(e) => {
+		m = { x: e.clientX, y: e.clientY };
+	}}
+	role="presentation"
+/>
 
 <style>
 	div {
